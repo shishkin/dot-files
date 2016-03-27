b9cdd5abf2b3bf5a4d35585e172ff3e9be51e273
Sergey Shishkin <sergei.shishkin@gmail.com>
2016-03-27 21:42:28 +0800

    mac osx defaults

--- defaults.sh
+++ defaults.sh
@@ -15,6 +15,18 @@ defaults write com.apple.dock showhidden -bool true
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 
+# Only show active apps in dock
+defaults write com.apple.dock static-only -bool true
+
+# Put dock on the left side
+defaults write com.apple.Dock orientation -string left
+
+# Auto-hide dock
+defaults write com.apple.dock autohide -bool true
+
+# Make dock tiles smaller
+defaults write com.apple.dock tilesize -int 48
+
 for app in "Dock" "Finder"; do
     killall "${app}"
 done
