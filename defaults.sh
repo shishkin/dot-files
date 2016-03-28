d60056e08e29840a7bc25eba9a5106139660a60c
Sergey Shishkin <sergei.shishkin@gmail.com>
2016-03-28 23:22:54 +0800

    more defaults

--- defaults.sh
+++ defaults.sh
@@ -9,6 +9,15 @@ defaults write com.apple.finder CreateDesktop -bool false
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0
+
+# Show all file extensions
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+
+# Use column view in all Finder windows by default
+defaults write com.apple.finder FXPreferredViewStyle -string clmv
+
 # Make Dock icons of hidden applications translucent
 defaults write com.apple.dock showhidden -bool true
 
@@ -26,6 +35,10 @@ defaults write com.apple.dock autohide -bool true
 
 # Make dock tiles smaller
 defaults write com.apple.dock tilesize -int 48
+
+# Bottom right hot corner starts screen saver
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 # Menu bar clock format
 defaults write com.apple.menuextra.clock DateFormat -string "EEE HH:mm"
