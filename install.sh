f540a82e00b0fbd227f7e9188a392cef6ed78f74
Sergey Shishkin <sergei.shishkin@gmail.com>
2018-07-07 15:07:18 +0800

    Install neovim python module

--- install.sh
+++ install.sh
@@ -14,6 +14,8 @@ brew install \
   ripgrep \
   wget
 
+pip3 install neovim
+
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
