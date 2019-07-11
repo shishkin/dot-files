978de474a42f7176da4137f986802a56ceb6db31
Sergey Shishkin <sergei.shishkin@gmail.com>
2019-07-11 22:44:46 +0800

    Defaults

--- defaults.sh
+++ defaults.sh
@@ -1,36 +1,22 @@
 #!/usr/bin/env bash
 
-# Show hidden files
+# Finder
 defaults write com.apple.finder AppleShowAllFiles TRUE
-
 # Hide desktop icons
 defaults write com.apple.finder CreateDesktop -bool false
-
-# Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
+defaults write com.apple.finder QuitMenuItem -bool true
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0
-
-# Show all file extensions
 defaults write com.apple.finder AppleShowAllExtensions -bool true
-
 # Use column view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string clmv
 
-# Make Dock icons of hidden applications translucent
+# Dock
 defaults write com.apple.dock showhidden -bool true
-
-# Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
-
-# Put dock on the left side
 defaults write com.apple.Dock orientation -string left
-
-# Auto-hide dock
 defaults write com.apple.dock autohide -bool true
-
-# Make dock tiles smaller
 defaults write com.apple.dock tilesize -int 48
 
 # Bottom right hot corner starts screen saver
@@ -55,6 +41,11 @@ defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
 #Tap to click
 defaults -currentHost write -globalDomain com.apple.mouse.tapBehavior -int 1
+
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+# Apps
+defaults write com.google.Chrome.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE
 
 for app in "Dock" "Finder" "SystemUIServer"; do
     killall -HUP "${app}"
