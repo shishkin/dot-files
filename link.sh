f2896a0acdd407604bac797e7f73a614f0e80f54
Sergey Shishkin <sergei.shishkin@gmail.com>
2019-07-11 22:46:03 +0800

    link

--- link.sh
+++ link.sh
@@ -2,6 +2,6 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-git submodule update --init --recursive --remote $DIR
+(cd "${DIR}" && git submodule update --init --recursive)
 
 ln -fnsv $DIR/home/.[^.]* $HOME/
