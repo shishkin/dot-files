853a651d1364978858a08e7ca8e05eeafdc068d5
Sergey Shishkin <sergey@shishkin.org>
2024-01-25 15:20:03 +0100

    linux installer

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash -ex
+
+if [ command -v apt-get ] &>/dev/null; then
+  if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+    apt-get update -y
+  fi
+  apt-get install -y --no-install-recommends \
+    fish \
+    fzf \
+    lsd \
+    wget \
+    zoxide
+fi
+
+chsh -s $(which fish) $USER
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl https://mise.jdx.dev/install.sh | sh
+
+./link.sh
